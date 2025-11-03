886949284bd46080512adcb529df60912ea66282
is9belle <isabellee.leee@gmail.com>
2025-11-02 23:09:16 -0800

    created main functions: buildEncodingTree & generateCodes

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
-//
-// Created by Manju Muralidharan on 10/19/25.
-//
 #include <iostream>
 #include <fstream>
 #include <stack>
 #include <string>
 #include "heap.h"
-using namespace std;
 
 // Global arrays for node information
-const int MAX_NODES = 64;
+const static int MAX_NODES = 64;
 int weightArr[MAX_NODES];
 int leftArr[MAX_NODES];
 int rightArr[MAX_NODES];
@@ -91,22 +87,61 @@ int createLeafNodes(int freq[]) {
 int buildEncodingTree(int nextFree) {
     // TODO:
     // 1. Create a MinHeap object.
+    MinHeap *minHeap = new MinHeap();
     // 2. Push all leaf node indices into the heap.
+    for (int i = 0; i<nextFree; i++) {
+        minHeap->push(i,weightArr);
+    }
     // 3. While the heap size is greater than 1:
     //    - Pop two smallest nodes
     //    - Create a new parent node with combined weight
     //    - Set left/right pointers
     //    - Push new parent index back into the heap
+    while (minHeap->size > 1) {
+        int smallest1 = minHeap->pop(weightArr);
+        int smallest2 = minHeap->pop(weightArr);
+
+        int parent = weightArr[smallest1] + weightArr[smallest2];
+        weightArr[nextFree] = parent;
+        leftArr[nextFree] = smallest1;
+        rightArr[nextFree] = smallest2;
+        charArr[nextFree] = ' ';
+        minHeap->push(nextFree, weightArr);
+        nextFree++;
+    }
     // 4. Return the index of the last remaining node (root)
-    return -1; // placeholder
+    return minHeap->pop(weightArr); // placeholder
 }
 
 // Step 4: Use an STL stack to generate codes
 void generateCodes(int root, string codes[]) {
     // TODO:
     // Use stack<pair<int, string>> to simulate DFS traversal.
+    stack<pair<int, string>> codesStack;
+    codesStack.push({root, ""});
+
+    int current;
+    string currentString;
     // Left edge adds '0', right edge adds '1'.
-    // Record code when a leaf node is reached.
+    while (!codesStack.empty()) {
+        current = codesStack.top().first;
+        currentString = codesStack.top().second;
+        codesStack.pop();
+        if (leftArr[current] == -1 && rightArr[current] == -1) { //if it is a leaf node, record the code
+            char currentChar = charArr[current];
+            if (currentChar>='a' && currentChar<='z') {
+                codes[currentChar - 'a'] = currentString;
+            }
+        }
+        else { //if it is not a leaf, then push to children
+            if (rightArr[current] != -1) {
+                codesStack.push({rightArr[current], currentString+"1"});
+            }
+            if (leftArr[current] != -1) {
+                codesStack.push({leftArr[current], currentString+"0"});
+            }
+        }
+    }
 }
 
 // Step 5: Print table and encoded message
